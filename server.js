9cdcb120453ca9762796c3f90a91acaec562e8fe
Muhammad Junaid <muhammadjunaidhassan1@gmail.com>
2019-04-28 02:22:03 +0500

    second commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var port = process.env.PORT || 3000;
 
 
 const router = express.Router();
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
